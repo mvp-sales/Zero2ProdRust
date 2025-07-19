619103d4afca20c702a30fda3de91b3c1ed12367
Marcus Sales <old.marcussales@gmail.com>
2025-07-19 01:48:10 +0100

    Finish Chapter 3.9

--- src/startup.rs
+++ src/startup.rs
@@ -2,13 +2,20 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
 use std::net::TcpListener;
+use sqlx::PgPool;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+) -> Result<Server, std::io::Error> {
     let port = listener.local_addr()?.port();
-    let server = HttpServer::new(|| {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
