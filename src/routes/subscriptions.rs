b6aa32e372328830cbb8c6a5ea366cd61bed3acf
Marcus Sales <old.marcussales@gmail.com>
2025-08-10 12:15:25 +0100

    Chapters 4.5.4 to 4.5.10

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[allow(dead_code)]
@@ -22,10 +23,7 @@ pub async fn subscribe(
         subscriber_name = %form.name
     );
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -39,6 +37,7 @@ pub async fn subscribe(
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
